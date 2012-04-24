5466aa07f4d11ffb2ceedbc470a0bafc75db3164
Gromila <gromilich@gmail.com>
2012-04-24 22:10:29 +0300

    upd

---
+++ 2 sem/2/Calculation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab
+{
+    class Calculation
+    {
+        private const double eps = 1e-12;
+
+        public static void Standart (double x, double low, double high, double step)
+        {
+            int k = Convert.ToInt32(Math.Floor((high - low)/step));
+            double[] y = new double[k+3];
+            int i = 1;
+            y[0] = Math.Cos(x);
+            Console.WriteLine("{0}. When x = {1}, y = {2}", 1, x, y[0]);
+            x = low;
+            while (x <= high)
+            {
+                y[i] = Math.Cos(x);
+                Console.WriteLine("{0}. When x = {1}, y = {2}", i+1, x, y[i]);
+                x += step;
+                i++;
+            }
+        }
+
+        static double Factorial (int n)
+        {
+            return (n == 0 ? 1 : Factorial(n - 1));
+        }
+
+        public static void Taylor (double x, double low, double high, double step, double e)
+        {
+            int k = Convert.ToInt32(Math.Floor((high - low) / step));
+            int i;
+            double a, s;
+            x = low;
+            do
+            {
+                a = 1;
+                i = 0;
+                s = 1;
+                while (Math.Abs(a) > e)
+                {
+                    i++;
+                    a *= -Math.Pow(x, 2*i)/Factorial(2*i);
+                    s += a;
+                }
+                Console.WriteLine("When x = {0}, y = {1}", x, s);
+                x += step;
+            } while (x < (high + step/2));
+        }
+    }
+}
---
+++ 2 sem/2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   if ((args.Length < 12) && (args.Length > 0))
+                   {
+                        double x;
+                        if (Double.TryParse(args[0], out x))
+                        {
+                            string type = "std";
+                            double low = 1.0;
+                            double high = 2.0;
+                            double step = 0.5;
+                            double e = 0.001;
+                            for (int i = 1; i < args.Length; i++)
+                            {
+                                switch (args[i])
+                                {
+                                    case "/type":
+                                        type = args[i+1];
+                                        i++;
+                                        break;
+                                    case "/low":
+                                        low = Double.Parse(args[i+1]);
+                                        i++;
+                                        break;
+                                    case "/high":
+                                        high = Double.Parse(args[i+1]);
+                                        i++;
+                                        break;
+                                    case "/step":
+                                        step = Double.Parse(args[i+1]);
+                                        i++;
+                                        break;
+                                    case "/e":
+                                        e = Double.Parse(args[i+1]);
+                                        i++;
+                                        break;
+                                    default:
+                                        Console.WriteLine("Incorrect parameter {0}", args[i]);
+                                        break;
+                                }
+                            }
+                            Console.WriteLine("Parameters: type: {0}, x: {1}, low: {2}, high: {3}, step: {4}, e: {5}",type,x,low,high,step,e);
+                            if (type.Equals("std")) Calculation.Standart(x, low, high, step);
+                            if (type.Equals("taylor")) Calculation.Taylor(x, low, high, step, e);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failure input");
+                    }
+                    Console.WriteLine("");
+        }
+    }
+}
